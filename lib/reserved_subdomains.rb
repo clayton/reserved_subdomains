984e235b647cfe646631d3f4c78a958b984fdfd0
Clayton <clayton@claytonlz.com>
2020-02-22 12:37:02 -0700

    Initial commit

---
+++ lib/reserved_subdomains.rb
@@ -0,0 +1,6 @@
+require "reserved_subdomains/version"
+
+module ReservedSubdomains
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reserved_subdomains/version.rb
@@ -0,0 +1,3 @@
+module ReservedSubdomains
+  VERSION = "0.1.0"
+end
